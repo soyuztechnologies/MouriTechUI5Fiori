93ea2b00124a5da93b943dfe04da286b1d9411d8
nishan-nainsukha <75522323+nishan-nainsukha@users.noreply.github.com>
2021-09-20 10:11:56 +0530

    Update View1.controller.js
    t

--- Day 10/Exercise 3/View1.controller.js
+++ Day 10/Exercise 3/View1.controller.js
@@ -10,15 +10,7 @@ sap.ui.define([
 		formatter: Formatter,
 				onInit: function() {
 		},
-		onNext: function() {
-			//Step 1: I need to go to Mom
-			var oAppCon = this.getView().getParent();
-			//Step 2: Ask mom to go to View 2
-			oAppCon.to("idView2");
-		},
-		onOrder: function() {
-			alert("order has been confirmed");
-		},
+		
 		onDelete: function(oEvent) {
 			var oItemToBeDeleted = oEvent.getParameter("listItem");
 			//list object
